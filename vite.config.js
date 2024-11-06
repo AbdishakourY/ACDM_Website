66f2321ac65464c4886649a02e415f2a9d096f6c
AbdishakourY <Abdishakour.moyousuf@gmail.com>
2024-11-06 13:46:17 +0300

    updated vite file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',  // Adjust if deploying in a subdirectory, e.g., '/subdirectory/'
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es',  // Ensures Vite outputs ECMAScript modules
+      },
+    },
+  },
 })
