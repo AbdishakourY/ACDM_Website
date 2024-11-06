13ff18fb74dea6d107f6860d541fa2d091ec7aff
AbdishakourY <Abdishakour.moyousuf@gmail.com>
2024-11-06 14:18:48 +0300

    fixed dist folder issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Adjust if deploying in a subdirectory, e.g., '/subdirectory/'
+  base: './',  // Adjust if deploying in a subdirectory, e.g., '/subdirectory/'
   build: {
     rollupOptions: {
       output: {
